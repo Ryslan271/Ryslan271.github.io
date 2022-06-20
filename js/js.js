82a92e18db4e4e15252ca5e5317d02893fce1e98
Ryslan271 <57673799+Ryslan271@users.noreply.github.com>
2022-06-21 02:29:22 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,65 @@
+window.onload = function () {
+    document.getElementById('open').onclick = function() {
+            $(".nav_2").css({
+                "visibility": "unset",
+                "position": "absolute",
+                "margin-right": "0",
+                "right": "0",
+                "transition": ".5s"
+        })
+    };
+
+    document.getElementById('exit').onclick = function() {
+            $(".nav_2").css({
+                "visibility": "unset",
+                "position": "absolute",
+                "margin-right": "-100%",
+                "right": "0",
+                "transition": ".5s"
+        })
+        $(".header").css({
+            "background": "rgb(201, 201, 201)",
+            "transition": ".9s",
+            "margin-top": "-0.5vh",
+            "position": "fixed"
+        })
+    };
+
+    $(document).ready(function(){
+ 
+        $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+        $('.scrollup').fadeIn();
+        } else {
+        $('.scrollup').fadeOut();
+        }
+        });
+         
+        $('.scrollup').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+        });
+         
+        });
+}
+
+window.onscroll = function () {
+    window.addEventListener('scroll', function(e) {
+        var st = window.scrollY;
+        if(st > 0) {
+            $(".header").css({
+                "background": "rgb(201, 201, 201)",
+                "transition": ".9s",
+                "margin-top": "-0.5vh",
+                "position": "fixed"
+            })
+        } else {
+            $(".header").css({
+                "background": "white",
+                "transition": ".9s",
+                "margin-top": "0",
+                "position": "relative"
+            })
+        }
+    });
+};
